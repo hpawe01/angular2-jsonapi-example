8d7753cba96904bf03b21a80bac8e9364528dd91
Friedrich Pawelka <friedrich.pawelka@googlemail.com>
2017-11-22 13:43:12 +0000

    Add specific examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Datastore } from './datastore';
 
 import { Comment } from './models/comment';
 import { Post } from './models/post';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,17 @@ export class AppComponent implements OnInit{
   constructor(private datastore: Datastore) { }
   ngOnInit() {
     this.datastore.query(Post)
-      .subscribe((items) => console.log(items));
+      .subscribe((items) => console.log('Example 1) All posts:', items));
+    this.datastore.query(Comment, {
+        include: 'post',
+      })
+      .subscribe((items) => console.log('Example 2) All comments including post', items));
+    this.datastore.query(User, {
+        include: 'comments',
+      })
+      .subscribe((items) => console.log('Example 3) All users including comments', items));
+    this.datastore.findRecord(Post, '1')
+      .subscribe((item) => console.log('Example 4) Specific post', item));
+
   }
 }
